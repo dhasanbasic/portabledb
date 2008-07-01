1001f1210452ff8bb718f2be62f696f63b31502e
Dinko Hasanbasic <dinko.hasanbasic@gmail.com>
2008-07-01 12:02:50 +0000

    Created initial project skeleton (for the first layer: B-tree)

---
+++ libPortableDB/btmeta.h
@@ -0,0 +1,12 @@
+/*
+ * btmeta.h
+ *
+ *  Created on: Jul 1, 2008
+ *      Author: dinko
+ */
+
+#ifndef BTMETA_H_
+#define BTMETA_H_
+
+
+#endif /* BTMETA_H_ */
---
+++ libPortableDB/btree.h
@@ -0,0 +1,12 @@
+/*
+ * btree.h
+ *
+ *  Created on: Jul 1, 2008
+ *      Author: dinko
+ */
+
+#ifndef BTREE_H_
+#define BTREE_H_
+
+
+#endif /* BTREE_H_ */
